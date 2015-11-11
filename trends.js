e9a6aca02c1cae7e21dcb86d03954c50794f646d
rk23 <reed.kinning@gmail.com>
2015-11-11 14:25:45 -0800

    Potential solution - split on 'script', find in array

--- trends.js
+++ trends.js
@@ -1,11 +1,12 @@
 var XMLHttpRequest = require('xhr2');
 
-dateTimeReviver = function (key, value) {
+function dateReviver(key, value) {
     var a;
     if (typeof value === 'string') {
-        a = /\/Date\((\d*)\)\//.exec(value);
+        a = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)(Z|([+\-])(\d{2}):(\d{2}))$/.exec(value);
         if (a) {
-            return new Date(+a[1]);
+            return new Date(Date.UTC(+a[1], +a[2] - 1, +a[3], +a[4],
+                            +a[5], +a[6]));
         }
     }
     return value;
@@ -14,27 +15,37 @@ dateTimeReviver = function (key, value) {
 function reqListener() {
   var data = this.responseText;
   // console.log(data);
-  var array = data.match(/[^\s]+/g);
-  // var array = data.split(" ");
-  var chartData = new Array();
-  var flag = 0;
-
-  for( var i = 0; i < array.length; ++i){
-    if (array[i] === "chartData"){
-      flag = 1;
-    }
-    if (array[i] === "</script>"){
-      flag = 0;
-    }
-    if (flag === 1) {
-      chartData.push(array[i]);
-    }
+  // var array = data.match(/[^\s]+/g);
+  var array = data.split("script");
+  for (var i = 0; i < array.length; i++){
+    console.log(i + " " + array[i]);
   }
-  chartData.shift();
-  chartData.shift();
-  chartData = chartData.join(" ");
-  // var d = JSON.parse(chartData);
-  console.log(chartData);
+  // var chartData = new Array();
+  // var flag = 0;
+  //
+  // for( var i = 0; i < array.length; ++i){
+  //   //chartData is the variable I want to pull from the given response
+  //   console.log(array[i]);
+  //   if (array[i] === "chartData"){
+  //     flag = 1;
+  //   }
+  //   if (array[i] === "</script>"){
+  //     flag = 0;
+  //   }
+  //   if (flag === 1) {
+  //     chartData.push(array[i]);
+  //     console.log("Saved");
+  //   }
+  // }
+  //
+  // //Remove the '=' and ' ' from the collected string before the data I want
+  // chartData.shift();
+  // chartData.shift();
+  //
+  // //Join back to a string, reinsert a space
+  // chartData = chartData.join(" ");
+  // var d = JSON.parse(JSON.stringify(chartData));
+  // console.log(d);
 }
 
 function reqError(err) {
@@ -44,10 +55,6 @@ function reqError(err) {
 var oReq = new XMLHttpRequest();
 oReq.onload = reqListener;
 oReq.onerror = reqError;
-oReq.open('get', 'https://www.google.com/trends/trendsReport?hl=en-US&q=Java&cmpt=q&tz=Etc%2FGMT%2B8&tz=Etc%2FGMT%2B8&content=1', true);
-// oReq.open('get', 'https://www.yahoo.com', true);
-// oReq.setDisableHeaderCheck(true);
+oReq.open('get', 'https://www.google.com/trends/trendsReport?hl=en-US&q=republican&cmpt=q&tz=Etc%2FGMT%2B8&tz=Etc%2FGMT%2B8&content=1', true);
 oReq.setRequestHeader('withCredentials', 'true');
-// oReq.setRequestHeader('Access-Control-Allow-Credentials', 'true');
-// oReq.setRequestHeader('Cookie', 'PREF=ID=1111111111111111:FF=4:LD=en:TM=1445280039:LM=1445280176:GM=1:V=1:S=pqOdp9m6e6XUirNy');
 oReq.send();
